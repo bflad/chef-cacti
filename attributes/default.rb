8ccf06d9be01b1f1d8a3b4be6063d2c9dd66d209
Brian Flad <bflad417@gmail.com>
2013-12-29 18:10:10 -0500

    Alphabetization and whitespace formatting in default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,3 @@
-default['cacti']['version']     = '0.8.8a'
-default['cacti']['user']        = 'cacti'
-default['cacti']['group']       = 'apache'
 default['cacti']['cron_minute'] = '*/5'
 default['cacti']['db_file'] = value_for_platform(
   %w{ centos redhat } => {
@@ -10,6 +7,7 @@ default['cacti']['db_file'] = value_for_platform(
     'default' => '/etc/cacti/debian.php'
   }
 )
+default['cacti']['group'] = 'apache'
 default['cacti']['packages'] = value_for_platform(
   %w{ centos redhat } => {
     'default' => %w{ cacti net-snmp net-snmp-utils perl-LDAP perl-Net-SNMP php-ldap php-mysql php-pecl-apc php-snmp }
@@ -28,24 +26,25 @@ default['cacti']['poller_file'] = value_for_platform(
     'default' => '/usr/share/cacti/site/poller.php'
   }
 )
+default['cacti']['user'] = 'cacti'
+default['cacti']['version'] = '0.8.8a'
 
 # Apache2 attributes
 
-default['cacti']['apache2']['server_name']             = node['fqdn']
-default['cacti']['apache2']['server_aliases']          = [node['hostname']]
-default['cacti']['apache2']['conf_dir']                = '/etc/httpd/conf.d'
-default['cacti']['apache2']['doc_root']                = '/var/www/html'
+default['cacti']['apache2']['conf_dir'] = '/etc/httpd/conf.d'
+default['cacti']['apache2']['doc_root'] = '/var/www/html'
+default['cacti']['apache2']['server_aliases'] = [node['hostname']]
+default['cacti']['apache2']['server_name'] = node['fqdn']
 
 default['cacti']['apache2']['ssl']['certificate_file'] = '/etc/pki/tls/certs/localhost.crt'
-default['cacti']['apache2']['ssl']['chain_file']       = ''
-default['cacti']['apache2']['ssl']['force']            = false
-default['cacti']['apache2']['ssl']['enabled']          = true
-default['cacti']['apache2']['ssl']['key_file']         = '/etc/pki/tls/private/localhost.key'
+default['cacti']['apache2']['ssl']['chain_file'] = ''
+default['cacti']['apache2']['ssl']['enabled'] = true
+default['cacti']['apache2']['ssl']['force'] = false
+default['cacti']['apache2']['ssl']['key_file'] = '/etc/pki/tls/private/localhost.key'
 
 # Spine attributes
 
-default['cacti']['spine']['version']  = node['cacti']['version']
-default['cacti']['spine']['url']      = "http://www.cacti.net/downloads/spine/cacti-spine-#{node['cacti']['spine']['version']}.tar.gz"
+default['cacti']['spine']['version'] = node['cacti']['version']
 default['cacti']['spine']['checksum'] = '2226070cd386a4955063a87e99df2fa861988a604a95f39bb8db2a301774b3ee'
 default['cacti']['spine']['packages'] = value_for_platform(
   %w{ centos redhat } => {
@@ -55,3 +54,4 @@ default['cacti']['spine']['packages'] = value_for_platform(
     'default' => %w{ libsnmp-dev libssl-dev }
   }
 )
+default['cacti']['spine']['url'] = "http://www.cacti.net/downloads/spine/cacti-spine-#{node['cacti']['spine']['version']}.tar.gz"
