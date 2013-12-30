7b7effcfd7e9cd078be9908e530dd22dc1134d7e
Brian Flad <bflad417@gmail.com>
2013-12-30 02:53:01 -0500

    Add apt/EPEL to default recipe, fix database ordering in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,10 @@
 settings = Cacti.settings(node)
 
+include_recipe 'apt' if node['platform'] == 'ubuntu'
+include_recipe 'yum-epel' if node['platform_family'] == 'rhel'
 include_recipe 'mysql::client'
-include_recipe 'cacti::database' if settings['database']['host'] == 'localhost'
 include_recipe 'cacti::package'
+include_recipe 'cacti::database' if settings['database']['host'] == 'localhost'
 include_recipe 'cacti::configuration'
 include_recipe 'cacti::apache2'
 include_recipe 'cacti::cron'
