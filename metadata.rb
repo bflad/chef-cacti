8f47bbfac3e7b5c4499d11b123251e5be982260f
Brian Flad <bflad417@gmail.com>
2013-12-30 02:57:19 -0500

    Update metadata for 0.3.0 release

--- metadata.rb
+++ metadata.rb
@@ -2,16 +2,20 @@ name              'cacti'
 maintainer        'Brian Flad'
 maintainer_email  'bflad417@gmail.com'
 license           'Apache 2.0'
-description       'Cookbook for installing/configuring Cacti.'
-version           '0.2.0'
-recipe            'cacti', 'Empty recipe.'
-recipe            'cacti::server', 'Installs Cacti server.'
-recipe            'cacti::spine', 'Installs Spine for Cacti server.'
+description       'Cookbook for installing/configuring Cacti'
+version           '0.3.0'
+recipe            'cacti', 'Installs/configures Cacti'
+recipe            'cacti::apache2', 'Installs/configures Apache 2 and PHP for Cacti'
+recipe            'cacti::configuration', 'Configures Cacti configuration files'
+recipe            'cacti::cron', 'Installs Cacti polling cron entry'
+recipe            'cacti::database', 'Installs/configures Cacti MySQL server'
+recipe            'cacti::package', 'Installs Cacti via packages'
+recipe            'cacti::spine', 'Installs Spine for Cacti server'
 
-%w{ apache2 build-essential cron database mysql }.each do |d|
+%w{ apache2 apt build-essential cron database mysql yum-epel }.each do |d|
   depends d
 end
 
-%w{ centos redhat ubuntu }.each do |os|
+%w{ centos fedora redhat ubuntu }.each do |os|
   supports os
 end
